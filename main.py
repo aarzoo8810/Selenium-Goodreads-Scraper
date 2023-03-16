fb34345deff1fb14a03826db842bb83dd7e23b75
Aarzoo <r.zoo786md@gmail.com>
2023-03-16 20:54:41 +0530

    get genre and adds shebang

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/home/mushoku/developer/day-81-100-portofolio-project/day-92-goodreads-scraper/venv/bin/python
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
@@ -6,6 +8,7 @@ import time
 FIREFOX_DRIVER_PATH = "/home/mushoku/developer/geckodriver"
 GOODREADS_URL = "https://www.goodreads.com/book/show/22360832-is-it-wrong-to-try-to-pick-up-girls-in-a-dungeon-light-novels-vol-1"
 SEARCH_KEYWORD = "light novel"
+
 
 class GoodreadsScraper:
     def __init__(self):
@@ -24,29 +27,31 @@ class GoodreadsScraper:
                                          value="h1").text
         print(f"{title = }")
 
-        poster_img_div = self.driver.find_element(by=By.CLASS_NAME, value="BookCover")
-        poster_img_url = poster_img_div.find_element(By.CLASS_NAME, "ResponsiveImage").get_attribute("src")
+        poster_img_div = self.driver.find_element(
+            by=By.CLASS_NAME, value="BookCover")
+        poster_img_url = poster_img_div.find_element(
+            By.CLASS_NAME, "ResponsiveImage").get_attribute("src")
         print(f"{poster_img_url = }")
 
-        author_illustrator = self.driver.find_element(By.CLASS_NAME,
-                                          value="ContributorLinksList").text.replace("\n ", "").replace("\n", "")
-        print(f"{author_illustrator = }")
-        # author = author_illustrator[0].text
-        # illustrator = author_illustrator[1].text
-        # print(f"{author = }")
-        # print(f"{illustrator = }")
+        contributors = self.driver.find_element(By.CLASS_NAME,
+                                                      value="ContributorLinksList").text.replace("\n ", "").replace("\n", "")
+        print(f"{contributors = }")
         
-        description = self.driver.find_element(By.CLASS_NAME,
-                                               value="Formatted").get_attribute("innerHTML")
+
+        description = self.driver.find_element(
+            By.CLASS_NAME, value="Formatted").get_attribute("innerHTML")
+
         print(f"{description = }")
-        
-        genre_more_btn = self.driver.find_element(By.CLASS_NAME,
-                                                  value="Button__container")
-        # genre_more_btn.click()
-        
-        genre_list = [genre.text for genre in self.driver.find_elements(By.CSS_SELECTOR,
-                                               value="html body div#__next div.PageFrame.PageFrame--siteHeaderBanner main.PageFrame__main.BookPage div.BookPage__gridContainer div.BookPage__rightColumn div.BookPage__mainContent div.BookPageMetadataSection div.BookPageMetadataSection__genres ul.CollapsableList div.Button__container")]
+
+        genre_more_btn = self.driver.find_element(By.CSS_SELECTOR,
+                                                  value="html body div#__next div.PageFrame.PageFrame--siteHeaderBanner main.PageFrame__main.BookPage div.BookPage__gridContainer div.BookPage__rightColumn div.BookPage__mainContent div.BookPageMetadataSection div.BookPageMetadataSection__genres ul.CollapsableList div.Button__container")
+        genre_more_btn.click()
+
+        genre_list = [genre.text for genre in self.driver.find_elements
+                      (By.CLASS_NAME, value="BookPageMetadataSection__genreButton")]
+
         print(f"{genre_list = }")
+
 
 ob = GoodreadsScraper()
 ob.search()

aa243e1ba081f9259b89230e84d446f9b43f3d63
Aarzoo <r.zoo786md@gmail.com>
2023-04-28 17:59:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,26 +8,25 @@ import pandas
 driver_path = "/home/mushoku/developer/geckodriver"
 base_url = "https://www.goodreads.com/"
 shelf_url = "https://www.goodreads.com/shelf/show/light-novel"
-url_limit = 5
-
+url_limit = 55
 
 
 def scrape_book_links():
     """This function scrapes book details and returns as list of dictionaries of book details"""
     genre = "Light Novel"
     desired_language = "English"
-    
+
     print(">> opening browser....")
     service = Service(driver_path)
     driver = webdriver.Firefox(service=service)
     driver.get(shelf_url)
-    
+
     book_url_tags = driver.find_elements(
         By.CLASS_NAME, value="bookTitle")
-    
+
     book_url_list = [url_tag.get_attribute(
         "href") for url_tag in book_url_tags]
-    
+
     book_items = []
     for book_url in book_url_list:
         if book_url_list.index(book_url) > url_limit:
@@ -48,8 +47,11 @@ def scrape_book_links():
 
             # this time.sleep is used for giving time to load data after popup
             time.sleep(5)
-            genre_more_btn = driver.find_element(By.CSS_SELECTOR,
-                                                    value="html body div#__next div.PageFrame.PageFrame--siteHeaderBanner main.PageFrame__main.BookPage div.BookPage__gridContainer div.BookPage__rightColumn div.BookPage__mainContent div.BookPageMetadataSection div.BookPageMetadataSection__genres ul.CollapsableList div.Button__container").click()
+            try:
+                genre_more_btn = driver.find_element(
+                    By.CSS_SELECTOR, value=".BookPageMetadataSection__genres > ul:nth-child(1) > div:nth-child(3)").click()
+            except NoSuchElementException:
+                pass
 
             genre_list = [genre.text for genre in driver.find_elements(
                 By.CLASS_NAME, value="BookPageMetadataSection__genreButton")]
@@ -58,23 +60,28 @@ def scrape_book_links():
                 book_details_btn = driver.find_element(
                     By.CSS_SELECTOR, value="div.CollapsableList > div:nth-child(3)")
                 book_details_btn.click()
-                # some books don't have either isbn or language
+
+                isbn = None
+                language = None
                 try:
-                    # if isbn is not present then this line will give language
+                    # if a book doesn't have an isbn it will give language as output
                     isbn = driver.find_element(
-                        By.XPATH, "/html/body/div[1]/div/main/div[1]/div[2]/div[1]/div[2]/div[6]/div/span[2]/div[1]/span/div/dl/div[3]").text.strip()
+                        By.XPATH, "/html/body/div[1]/div/main/div[1]/div[2]/div[1]/div[2]/div[6]/div/span[2]/div[1]/span/div/dl/div[3]/dd/div/div[1]").text
                 except NoSuchElementException:
                     isbn = None
-                    language = None
                 else:
-                    if "Language" in isbn:
-                        language = isbn.split("Language\n")[-1]
-                        isbn = None
-                    else:
-                        isbn = isbn.split("ISBN\n")[-1]
+                    try:
                         language = driver.find_element(
-                            By.CSS_SELECTOR, "div.DescListItem:nth-child(4) > dd:nth-child(2) > div:nth-child(1) > div:nth-child(1)").text.strip()
-                
+                            By.CSS_SELECTOR, "div.DescListItem:nth-child(4) > dd:nth-child(2) > div:nth-child(1) > div:nth-child(1)").text
+                    except NoSuchElementException:
+                        if "isbn" not in isbn:
+                            language = isbn
+                            isbn = None
+                        else:
+                            language = None
+
+                print(f"{isbn = }")
+                print(f"{language= }")
                 if desired_language == language:
                     title = driver.find_element(
                         by=By.TAG_NAME, value="h1").text
@@ -88,42 +95,45 @@ def scrape_book_links():
                         By.CLASS_NAME, value="Formatted").get_attribute("innerHTML")
                     page_edition = driver.find_element(
                         By.CSS_SELECTOR, value=".FeaturedDetails > p:nth-child(1)").text
-                    
+
                     num_of_pages = None
                     edition = page_edition
                     if "pages" in page_edition:
                         num_of_pages, edition = page_edition.split(" pages, ")
-                        
+
                     if num_of_pages:
                         num_of_pages = int(num_of_pages)
-                        
-                    published_date = driver.find_element(By.CSS_SELECTOR, value=".FeaturedDetails > p:nth-child(2)").text.split("published ")[-1]
-                    recommended_book_section = driver.find_element(By.CSS_SELECTOR, ".BookPage__relatedTopContent > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1)")
-                    recommended_book_urls = [anchor_tag.get_attribute("href") for anchor_tag in recommended_book_section.find_elements(By.TAG_NAME, "a")]
-                    
+
+                    published_date = driver.find_element(
+                        By.CSS_SELECTOR, value=".FeaturedDetails > p:nth-child(2)").text.split("published ")[-1]
+                    recommended_book_section = driver.find_element(
+                        By.CSS_SELECTOR, ".BookPage__relatedTopContent > div:nth-child(1) > div:nth-child(1) > section:nth-child(1) > div:nth-child(2) > div:nth-child(1)")
+                    recommended_book_urls = [anchor_tag.get_attribute(
+                        "href") for anchor_tag in recommended_book_section.find_elements(By.TAG_NAME, "a")]
+
                     for url in recommended_book_urls:
                         if url not in book_url_list and len(book_url_list) < url_limit:
                             book_url_list.append(url)
                             print(len(book_url_list))
-                            
-                    
+
                     book_item = {"title": title,
-                                "book_cover_url": book_cover_url,
-                                "contributors": contributors,
-                                "genre": genre_list,
-                                "description": description,
-                                "num_of_pages": num_of_pages,
-                                "edition": edition,
-                                "isbn": isbn,
-                                "language": language,
-                                "published_date": published_date,
-                                }
+                                 "book_cover_url": book_cover_url,
+                                 "contributors": contributors,
+                                 "genre": genre_list,
+                                 "description": description,
+                                 "num_of_pages": num_of_pages,
+                                 "edition": edition,
+                                 "isbn": isbn,
+                                 "language": language,
+                                 "published_date": published_date,
+                                 }
                     book_items.append(book_item)
                     print(book_item)
-                    
+
+                    print(f"{book_items = }")
+                    print(f"{len(book_items) = }")
                 else:
                     print("Did Not Scrape This URL Becuase Language does not match")
-
 
     return book_items
 
@@ -136,5 +146,3 @@ def save_data_to_file(book_items):
 
 book_items = scrape_book_links()
 save_data_to_file(book_items=book_items)
-
-
